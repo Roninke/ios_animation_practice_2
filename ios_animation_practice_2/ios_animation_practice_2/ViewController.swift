8996bdc25443cba953f24acfc21d665a9666b9a6
Roninke <ceo.roninke@gmail.com>
2016-09-26 16:59:43 +0800

    Initial project

---
+++ ios_animation_practice_2/ios_animation_practice_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_animation_practice_2
+//
+//  Created by Ronin Ke on 2016/9/26.
+//  Copyright © 2016年 Ronin Ke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
